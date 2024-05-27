bf79bc279217863ac91e193a25d9877132c175b6
samenoko-112 <kumagai7554@gmail.com>
2024-05-28 01:54:27 +0900

    オプションの追加

--- main.py
+++ main.py
@@ -24,6 +24,20 @@ def main(page:Page):
     page.window_height = 800
     page.padding = 16
 
+    mp4_quality = [dropdown.Option(key="Auto"),dropdown.Option(key="1080p"),dropdown.Option(key="720p"),dropdown.Option(key="480p"),dropdown.Option(key="360p"),dropdown.Option(key="240p"),dropdown.Option(key="144p")]
+    mp3_quality = [dropdown.Option(key="Auto"),dropdown.Option(key="320kbps"),dropdown.Option(key="128kbps")]
+
+    def change_options(e):
+        if mode_sel.value == "mp4":
+            quality_sel.options = mp4_quality
+            quality_sel.update()
+        elif mode_sel.value == "mp3":
+            quality_sel.options = mp3_quality
+            quality_sel.update()
+        else:
+            quality_sel.options = []
+            quality_sel.update()
+
     def sel_path(e: FilePickerResultEvent):
         global output_path
         before = output_path
@@ -31,6 +45,14 @@ def main(page:Page):
         path_input.value = output_path
         path_input.update()
         return
+    
+    def display_multi(e):
+        if use_multi.value == True:
+            multi_threads.visible = True
+            multi_threads.update()
+        elif use_multi.value == False:
+            multi_threads.visible = False
+            multi_threads.update()
     
     def sel_cookie(e: FilePickerResultEvent):
         global cookie_file
@@ -62,7 +84,7 @@ def main(page:Page):
             if mode_sel.value == "mp4":
                 command.extend(['--merge-output-format','mp4'])
                 command.append('-f')
-                if quality_sel.value == "自動":
+                if quality_sel.value == "Auto":
                     command.append('bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]')
                 elif quality_sel.value == "1080p":
                     command.append('bestvideo[ext=mp4][height<=1080]+bestaudio[ext=m4a]/best[ext=mp4][height<=1080]')
@@ -72,14 +94,18 @@ def main(page:Page):
                     command.append('bestvideo[ext=mp4][height<=480]+bestaudio[ext=m4a]/best[ext=mp4][height<=480]')
                 elif quality_sel.value == "360p":
                     command.append('bestvideo[ext=mp4][height<=360]+bestaudio[ext=m4a]/best[ext=mp4][height<=360]')
+                elif quality_sel.value == "240p":
+                    command.append('bestvideo[ext=mp4][height<=240]+bestaudio[ext=m4a]/best[ext=mp4][height<=240]')
+                elif quality_sel.value == "144p":
+                    command.append('bestvideo[ext=mp4][height<=144]+bestaudio[ext=m4a]/best[ext=mp4][height<=144]')
             elif mode_sel.value == "mp3":
-                command.append('-f')
-                command.append('bestaudio')
-                command.append('-x')
-                command.append('--audio-format')
-                command.append('mp3')
-                command.append('--audio-quality')
-                command.append('256K')
+                command.extend(['-f','bestaudio/best','-x'])
+                if quality_sel.value == "Auto":
+                    command.extend(['--audio-format', 'mp3'])
+                elif quality_sel.value == "320kbps":
+                    command.extend(['--audio-format', 'mp3', '--audio-quality', '320K'])
+                elif quality_sel.value == "128kbps":
+                    command.extend(['--audio-format', 'mp3', '--audio-quality', '128K'])
             elif mode_sel.value == "wav":
                 command.append('-f')
                 command.append('bestaudio')
@@ -87,7 +113,7 @@ def main(page:Page):
                 command.append('--audio-format')
                 command.append('wav')
             if use_multi.value == True:
-                command.extend(['-N','8'])
+                command.extend(['-N',f'{multi_threads.value}'])
             else:
                 pass
             if playlist.value == True and name_index.value == False:
@@ -180,12 +206,13 @@ def main(page:Page):
     dl_btn = FloatingActionButton("ダウンロード",icon=ft.icons.DOWNLOAD,on_click=download)
     cookie_input = TextField(value=cookie_file, hint_text="Cookieファイルを選択", label="Cookie", icon=ft.icons.COOKIE, expand=True, read_only=True)
     cookie_btn = ft.TextButton("選択", icon=ft.icons.COOKIE, on_click=lambda _:cookie_select.pick_files(allowed_extensions=["txt"]))
-    mode_sel = Dropdown(value="mp4",label="フォーマット",options=[dropdown.Option("mp4"),dropdown.Option("mp3"),dropdown.Option("wav")],expand=True)
-    quality_sel = Dropdown(value="自動",label="画質(mp4のみ)",options=[dropdown.Option("自動"),dropdown.Option("1080p"),dropdown.Option("720p"),dropdown.Option("480p"),dropdown.Option("360p")],expand=True)
+    mode_sel = Dropdown(value="mp4",label="フォーマット",options=[dropdown.Option("mp4"),dropdown.Option("mp3"),dropdown.Option("wav")],expand=True,on_change=change_options)
+    quality_sel = Dropdown(label="品質",options=mp4_quality,expand=True)
     log_out = Text(value=dl_log,max_lines=1,)
     progress_bar = ft.ProgressBar(value=0)
     name_index = Switch(label="プレイリストのインデックスをファイル名に含める",tooltip=f"ファイル名にプレイリストのインデックスを含めます。")
-    use_multi = Switch(label="同時接続する",tooltip=f"同時接続して高速でダウンロードできるようにします\nエラーが発生する可能性があります",expand=True)
+    use_multi = Switch(label="同時接続する",tooltip=f"同時接続して高速でダウンロードできるようにします\nエラーが発生する可能性があります",expand=True,on_change=display_multi)
+    multi_threads = ft.Slider(value=8,max=20,min=1,divisions=20,visible=False,tooltip="{value}")
     emb_thumbnail = Switch(label="サムネイルを埋め込む",tooltip=f"ファイルにサムネイルを埋め込みます")
     playlist = Switch(label="プレイリスト名でフォルダを作成",tooltip=f"プレイリストの名前でフォルダを作成し、その中にファイルを保存します\nプレイリストでない場合はNAに保存されます")
     use_aria2 = Switch(label="Aria2を使う",tooltip=f"外部ダウンローダーであるAria2を使用します。\n同時接続数を増やし高速でダウンロードできるようにします\n接続が制限され極端に速度が低下する可能性があります",expand=True)
@@ -200,6 +227,7 @@ def main(page:Page):
         name_index,
         emb_thumbnail,
         ft.Row([use_multi,use_aria2]),
+        multi_threads,
         ft.Row([cookie_input, cookie_btn]),
         Text("ログ",size=18),
         log_out,
