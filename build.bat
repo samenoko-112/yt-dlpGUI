e5cef4602cf41c5d74126e581f0316702bc46d84
samenoko-112 <kumagai7554@gmail.com>
2024-06-26 01:30:01 +0900

    ビルドスクリプト、requirements.txtの更新

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 @echo off
-flet build windows . --product "yt-dlpGUI" --project "yt-dlpGUI"
+call venv\Scripts\activate
+nuitka --windows-console-mode=disable --standalone --onefile --output-filename=yt-dlpGUI.exe main.py
 pause
